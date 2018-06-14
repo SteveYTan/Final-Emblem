9472ce90d998ce68f8a0bd030e13bc486c4c52d3
Steve Tan <Steveytan@gmail.com>
2018-06-14 14:59:52 -0400

    more changes to ports/heroku

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, './static')));
 
-app.listen(5000, function(){
-	console.log('listening on port 5000');
-})
+// app.listen(5000 || process.env.PORT, function(){
+// 	console.log('listening on port 5000');
+// })
+
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });

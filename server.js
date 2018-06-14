83396dde57cb3cc8078b6a3f3f8e3846d85e5b9b
Steve Tan <Steveytan@gmail.com>
2018-06-14 14:51:05 -0400

    fixed port number

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, './static')));
 
-app.listen(8000, function(){
+app.listen(5000, function(){
 	console.log('listening on port 5000');
 })
